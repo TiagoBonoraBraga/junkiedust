0b8d137ccfe7490514e15bfe2a0ddd7c8ea21948
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2023-11-23 13:05:59 -0300

    feat: add color and screen config intailwind.config.ts and change nav color

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -3,7 +3,7 @@ import CustomLogo from "../atoms/CustomLogo";
 export default function Header(){
     const [navbar, setNavbar] = useState(false);
     return (
-        <nav className="w-full bg-blue-300 shadow">
+        <nav className="w-full bg-purple-500 shadow">
         <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
