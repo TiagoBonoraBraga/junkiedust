5ca03082dc670054fdee4a438a59fcb9ea8071cb
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2023-11-23 13:05:59 -0300

    feat: add color and screen config intailwind.config.ts and change nav color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,33 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      ss: { max: '1030px'},
+      s:  '820px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    colors: {
+      black:{
+        500: '#000000',
+      },
+      white: {
+        500: '#ffffff',
+      },
+      purple: {
+        500: '#800080'
+      },
+      blue: {
+        100: '#6274e5',
+        200: '#4a64d2',
+        300: '#3155c0',
+        400: '#1945ad',
+        500: '#00359a',
+      },
+    
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
