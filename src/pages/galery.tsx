f606275bc956e4667e9cf677d2bdcf57b197de9d
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2023-11-23 13:08:26 -0300

    feat: create galery page and add Layout template

---
+++ src/pages/galery.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/templates/Layout"
+
+
+export default function Galery() {
+  return (
+    <main
+      className='flex min-h-screen flex-col items-center justify-between p-5 '
+    >
+     <Layout>
+      
+     </Layout>
+    </main>
+  )
+}
