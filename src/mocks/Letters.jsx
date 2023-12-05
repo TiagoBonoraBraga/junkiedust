4431853e2563da9ae0f3303c9f2009ec21a0be65
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2023-12-04 22:02:30 -0300

    feat: update mocks

--- src/mocks/Letters.jsx
+++ src/mocks/Letters.jsx
@@ -7,11 +7,7 @@ export const letters = [
     {
         title: 'Lighter and Dust',
         letter: 'When the sky is falling down raining knives from the clouds needing a shelter conjuring for the thunder comes the lightning shining your way then you can see above but still in doubt witch road you can drive lightning and thunder lightning and thunder lightning and thunder lightning and thunder keeping forward rocks falling and you need a shelter lightning and thunder gonna be your salvation lightning and thunder lightning and thunder lightning and thunder lightning and thunder'
-    },
-    {
-        title: "Junkie Dust",
-        letter: 'Walking down in the street at the night Looking for something that could be alright I wasnt expecting him to come over But he comes anyway This wasnt what Im looking for Have a lot of questions in my mind Thats the kind of thing I shouldnt have done But keeps chasing me In the Junkie Dust Its been a long time since i havent play with Ordinary people want to know? How to deal I cant answer the question even for myself Because the Junkie Dust is only a secret place for each one I went back to the same place i came from All the questions are gone That is the kind of thing i always do When i find myself In the junkie dust In order to do that what really needs to be done Who is in charge In charge of your life '
-    },
+    },   
     {
         title: 'Finding Dust',
         letter: 'In life i always try to find you lost in time of love seeing you like someone someone untouchable where are you How i can find where are you lost in my mind finding love finding love where you are finding live why you take so long to find me why you take so long to take me away from darkness your love stand me keep me surviving i cant lose you nevermore i found you please still still on my side on my side we gonna be together forever good times bad times sharing all we can loving you forever '
@@ -19,7 +15,11 @@ export const letters = [
     {
         title: 'Leave this Dust',
         letter: 'We live trying trying to live This Way This body is just a superfluous thing like a prison of our spirit Trying to get out Other worlds expecting us travel for all universe In form of energy Gonna Find elixir Get free from this form Past and future doesnt make difference Present is a black hole Time is just a joke.'
-    }
+    },
+    {
+        title: "Junkie Dust",
+        letter: 'Walking down in the street at the night Looking for something that could be alright I wasnt expecting him to come over But he comes anyway This wasnt what Im looking for Have a lot of questions in my mind Thats the kind of thing I shouldnt have done But keeps chasing me In the Junkie Dust Its been a long time since i havent play with Ordinary people want to know? How to deal I cant answer the question even for myself Because the Junkie Dust is only a secret place for each one I went back to the same place i came from All the questions are gone That is the kind of thing i always do When i find myself In the junkie dust In order to do that what really needs to be done Who is in charge In charge of your life '
+    },
 
     
    
