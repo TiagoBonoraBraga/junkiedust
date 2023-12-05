05b80e4ea63031648bdeb47226fe25a90264df7d
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2023-12-04 21:24:19 -0300

    feat: create mocks letters

---
+++ src/mocks/Letters.jsx
@@ -0,0 +1,12 @@
+export const letters = [
+    {
+        title: 'Planet trouble',
+        letter: 
+        'You live, in a world full of sadness\n Sickness and disease\n What you gonna do \nFor still\n In this planet trouble \n Planet trouble What you gonna do Planet trouble Want you runaway?\nThe world still full of problems\n And gotta nothing that you can do\nJust accept your life \n In the Way you can live\n Planet trouble \n What you gonna do \n Planet trouble \n Want you runaway?\n  If you keep on your on\n No help even for yourself\n cant find the cure\ncant find the cure\nPlanet trouble\nWhat you gonna do\nPlanet trouble \nWant you runaway? '        
+    },
+    {
+        title: 'lighter and thunder',
+        letter: 'When the sky is falling down\nraining knives from the clouds\nneeding a shelter \nconjuring for the thunder comes\nthe lightning shining your way\nthen you can see above\nbut still in doubt\nwitch road you can drive\nlightning and thunder\nlightning and thunder\nlightning and thunder\nlightning and thunder\nkeeping forward\nrocks falling and you need a shelter\nlightning and thunder\ngonna be your salvation\nlightning and thunder\nlightning and thunder\nlightning and thunder\nlightning and thunder'
+    },
+   
+]
