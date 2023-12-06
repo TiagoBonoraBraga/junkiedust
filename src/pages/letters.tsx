c7ba238f9884a3bc360c6ca4966cc9aec26478e1
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2023-12-06 19:57:44 -0300

    feat: change global style bg-color

--- src/pages/letters.tsx
+++ src/pages/letters.tsx
@@ -2,10 +2,10 @@ import Layout from "@/components/templates/Layout";
 import LetterCard from "@/components/organisms/LetterCard"
 export default function Letters() {
     return (
-        <div className="bg-black">
-            <Layout>
-                <LetterCard />
-            </Layout>
-        </div>
+
+        <Layout>
+            <LetterCard />
+        </Layout>
+
     )
 }
