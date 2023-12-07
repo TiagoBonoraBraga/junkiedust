22a33a9d8eb8ccf4045c74acd80828e036553b15
Junkie Dust <junkiedust@gmail.com>
2023-12-07 20:49:55 -0300

    New Line

--- src/mocks/letters.jsx
+++ src/mocks/letters.jsx
@@ -2,7 +2,7 @@ export const letters = [
     {
         title: 'Planet Dust',
         letter: 
-        'You live, in a world full of sadness Sickness and disease  What you gonna do For still In this planet trouble Planet trouble What you gonna do Planet trouble Want you runaway? The world still full of problems And gotta nothing that you can do Just accept your life In the Way you can live Planet trouble What you gonna do Planet trouble Want you runaway? If you keep on your on No help even for yourself cant find the cure cant find the cure Planet trouble What you gonna do Planet trouble Want you runaway? '        
+        'You live, in a world full of sadness Sickness and disease \n What you gonna do For still In this planet trouble \n Planet trouble What you gonna do Planet trouble Want you runaway? The world still full of problems And gotta nothing that you can do Just accept your life In the Way you can live Planet trouble What you gonna do Planet trouble Want you runaway? If you keep on your on No help even for yourself cant find the cure cant find the cure Planet trouble What you gonna do Planet trouble Want you runaway? '        
     },
     {
         title: 'Lighter and Dust',
