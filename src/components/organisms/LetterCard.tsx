d1802ae728d101ebe81871276dcf64cde7327a6e
TiagoBonoraBraga <ogait.desenvolvedor.jr@gmail.com>
2023-12-09 18:19:21 -0300

    feat: try format letter

--- src/components/organisms/LetterCard.tsx
+++ src/components/organisms/LetterCard.tsx
@@ -1,18 +1,19 @@
 import { letters } from '@/mocks/letters';
 
+
+
 export default function LetterCard() {
     return (
         <>
             <div className='mt-20  px-3 flex flex-wrap justify-center items-center ss:flex ss:flex-col ss:px-0 gap-5'>
-                {letters.map((letter, index) => (
+                {letters?.map((letter, index) => (
                     <section key={index} className="w-1/3 ring-2 ring-purple-500 rounded-md ss:w-80 mb-10">
 
                         <div className="bg-purple-500 text-white flex flex-row justify-center items-center">
                             {letter.title}
-
                         </div>
                         <div className="flex flex-row justify-center items-center p-5 bg-white rounded-b-lg">
-                            {letter.letter}
+                         {letter.letter}
                         </div>
                     </section>
                 ))}
--- src/lib/functions.js
+++ src/lib/functions.js
@@ -1,9 +1,9 @@
-function NewlineText(props) {
+export default function NewlineText(props) {
   const text = props.text;
   const newText = text.split('\n').map(str => <p>{str}</p>);
   
   return newText;
 }
 
-export NewlineText;
+
 
